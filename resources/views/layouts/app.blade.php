1467712cd528621a9f17e35c3e2ea8a5bad106d6
fmorales <fmorales@T460S-FMORALES.kaufmann.cl>
2017-08-01 22:54:59 -0400

    home del sitio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     </head>
     <body>
         <div id="app">
-            <nav class="navbar navbar-default navbar-static-top">
+            <nav class="navbar navbar-inverse navbar-static-top">
                 <div class="container">
                     <div class="navbar-header">
 
@@ -32,7 +32,7 @@
 
                         <!-- Branding Image -->
                         <a class="navbar-brand" href="{{ url('/') }}">
-                            {{ config('app.name', 'Laravel') }}
+                            Cotización App
                         </a>
                     </div>
 
---
+++ resources/views/main/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Cotizacion App')
+
+@section('content')
+
+    <h1>Bienvenidos</h1>
+@endsection
